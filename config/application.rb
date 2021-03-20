e388dad9d4907fccc64cb5ecc30dd1d10cf0571b
chloethedog <hitomiandsakura@hotmail.com>
2021-03-20 15:10:15 +0900

    flash sign

--- config/application.rb
+++ config/application.rb
@@ -15,10 +15,11 @@ module ChatSpace2
       g.javascripts false
       g.helper false
       g.test_framework false
-    end  
+    end 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.i18n.default_locale = :ja
   end
 end
